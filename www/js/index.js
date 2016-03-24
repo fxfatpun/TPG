463ec9ed07d973f1163f29a909c9aa3943bc7c40
fxfatpun <fxfatpun@gmail.com>
2016-03-24 11:09:41 +0800

    bug fix

--- www/js/index.js
+++ www/js/index.js
@@ -48,13 +48,13 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-    }
+    },
 	
 	showAlert: function (message, title) {
-    if (navigator.notification) {
-        navigator.notification.alert(message, null, title, 'OK');
-    } else {
-        alert(title ? (title + ": " + message) : message);
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
     }
-},
 };
